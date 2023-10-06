9987eff7ba93a9e4e1e82e6aeb544f552f63f6a2
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-10-06 07:44:24 -0600

    added the object codes

--- client.js
+++ client.js
@@ -3,8 +3,15 @@ const typesOfFood = ["Chicken", "BBQ", "Ice Cream", "Yogurt"];
 
 console.log(typesOfFood)
 
-const newFood = typesOfFood.push("Peanuts")
+/* This line of code pushes a new string into the array we have. This mutates the original array as it is adding a new thing into the array.*/
+const newFood = typesOfFood.push("Peanuts" , "Strawberry")
 console.log(typesOfFood)
 
+/* This line of code removes a specific string from the array with the syntax of pop. This method does mutate the array as it removes something from it permanently. */
 const remFood = typesOfFood.pop("Peanuts")
 console.log(typesOfFood)
+/* Both pop and push mutate, that is why we return the value of the original variable because it has been permanently changed. */
+
+/* This includes syntax returns a true or false statement if the indicated string is within that array */
+const specFood = typesOfFood.includes("BBQ")
+console.log(specFood)
