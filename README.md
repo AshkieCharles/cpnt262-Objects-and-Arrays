4d5be6fba35983120eae955bcf61fd56b6e4a853
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-10-06 08:07:53 -0600

    finished the remaining object code

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 ### Activity Name: Graded Activity 3 - Objects and Arrays
 ### Author: Ashkie Esperanza
 ### Attributions:
+- [Objects](https://www.w3schools.com/js/js_object_definition.asp)
+- [Delete](https://youtu.be/KDOHsbC-bMI?si=DrYNpZ2AM-J_Jslm)
+
 ### Code Journal:
+- Trying to remember what lists and dictionaries are from python really helped me through this as it is the exact same thing but with different syntax.
--- client.js
+++ client.js
@@ -1,17 +1,23 @@
-let age = 18;
+let userInfo = {Name:'Ashkie', Age: 18, School: 'SAIT'}
 const typesOfFood = ["Chicken", "BBQ", "Ice Cream", "Yogurt"];
-
+console.log(userInfo)
 console.log(typesOfFood)
 
 /* This line of code pushes a new string into the array we have. This mutates the original array as it is adding a new thing into the array.*/
 const newFood = typesOfFood.push("Peanuts" , "Strawberry")
 console.log(typesOfFood)
 
-/* This line of code removes a specific string from the array with the syntax of pop. This method does mutate the array as it removes something from it permanently. */
+/* This line of code removes a specific string from the array. This method does mutate the array as it removes something from it permanently. */
 const remFood = typesOfFood.pop("Peanuts")
 console.log(typesOfFood)
 /* Both pop and push mutate, that is why we return the value of the original variable because it has been permanently changed. */
 
-/* This includes syntax returns a true or false statement if the indicated string is within that array */
+/* This does not mutate the original array as it simply returns a yes or no and doesnt change the array itself*/
 const specFood = typesOfFood.includes("BBQ")
 console.log(specFood)
+
+let valUser = Object.values(userInfo)
+console.log(valUser)
+
+let delKey = delete userInfo['School']
+console.log(userInfo)
