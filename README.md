6aa82e06382427e9abc85cd8e372a39249da3ef4
Ashkie Esperanza <ashkiecharles@gmail.com>
2023-10-06 07:30:31 -0600

    added .pop and .push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cpnt262-Objects-and-Arrays
+### Activity Name: Graded Activity 3 - Objects and Arrays
+### Author: Ashkie Esperanza
+### Attributions:
+### Code Journal:
---
+++ client.js
@@ -0,0 +1,10 @@
+let age = 18;
+const typesOfFood = ["Chicken", "BBQ", "Ice Cream", "Yogurt"];
+
+console.log(typesOfFood)
+
+const newFood = typesOfFood.push("Peanuts")
+console.log(typesOfFood)
+
+const remFood = typesOfFood.pop("Peanuts")
+console.log(typesOfFood)
